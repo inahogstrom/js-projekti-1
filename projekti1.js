408187e5e0cf0daf86dbdbb4ca6195f879ae66fe
InaHögström <ina.hogstrom@gmail.com>
2021-10-15 11:31:27 +0300

    js aloittelua

--- projekti1.js
+++ projekti1.js
@@ -1,5 +1,10 @@
-  function store(){
+/* function store(){
      var inputEmail= document.getElementById("email");
      localStorage.setItem("email", inputEmail.value);
     }
     function SubmitClick() { $("#submit").click(function(event) { alert("it works!"); }); } $(document).ready(function() { SubmitClick(); });
+    */
+
+function validateFields() {
+    var tunnit = 0;
+}
